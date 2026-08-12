63bf1003c6714880cf3198fd77b544546e8f4daa
Hitesh <hiteshmankar777@gmail.com>
2026-08-12 17:58:46 +0530

    adD HITESH

---
+++ NEW-PRATICE-HITESH/seciptedfile.groovy
@@ -0,0 +1,17 @@
+node {  
+    stage('PULL') { 
+        echo 'Source COde PULL'
+    }
+    stage('BUILD') { 
+        echo 'Application BUILD'
+    }
+    stage('TEST') { 
+        echo 'TEST Success'
+    }
+    stage('DEPLOY') { 
+        echo 'Deploy DOne'
+    }
+    stage('MONITOR') { 
+        echo 'Deploy DOne'
+    }
+}
